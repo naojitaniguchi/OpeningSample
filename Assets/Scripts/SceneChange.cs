4764644e0fd8d6875e2e4febda1f46c6adace855
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-09-30 10:13:00 +0900

    First commit.

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public string SceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
